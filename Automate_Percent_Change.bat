d0678bc6745b795d635c09637b9ec1e26dc1cf4f
BalaPavan Kommareddy <54868461+kbpavan@users.noreply.github.com>
2020-03-20 11:43:00 -0500

    Delete Automate_Percent_Change.bat

--- Automate_Percent_Change.bat
+++
@@ -1,6 +0,0 @@
-
-C:\ProgramData\Anaconda3\python.exe ^
-C:\ProgramData\Anaconda3\cwp.py ^
-C:\ProgramData\Anaconda3 ^
-C:\ProgramData\Anaconda3\python.exe ^
-C:\Users\****\Desktop\Usage_prediction\Automation\Monthly_Percent_Change_UsageVsWeather.py
