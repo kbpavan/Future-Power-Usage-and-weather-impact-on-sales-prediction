2dab852c8f4dc719b108c85ebd18a60704d07481
BalaPavan Kommareddy <54868461+kbpavan@users.noreply.github.com>
2020-03-20 11:42:52 -0500

    Delete Automate_Future_Usage_Prediction.bat

--- Automate_Future_Usage_Prediction.bat
+++
@@ -1,6 +0,0 @@
-
-C:\ProgramData\Anaconda3\python.exe ^
-C:\ProgramData\Anaconda3\cwp.py ^
-C:\ProgramData\Anaconda3 ^
-C:\ProgramData\Anaconda3\python.exe ^
-C:\Users\***\Desktop\Usage_prediction\Automation\Automate_Future_Usage_Prediction.py
